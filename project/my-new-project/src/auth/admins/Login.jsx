b3671719d42a319da98e4c6d57e7a7f555d2d8ae
cfdaica123 <soncaodarkin3@gmail.com>
2023-09-08 18:59:54 +0700

    commit update

--- project/my-new-project/src/auth/admins/Login.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const LoginAmin = () => {
-  return (
-    <div>LoginAmin</div>
-  )
-}
-
-export default LoginAmin
--- project/my-new-project/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from 'react-dom';
-import App from "./app.js";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App/>)
